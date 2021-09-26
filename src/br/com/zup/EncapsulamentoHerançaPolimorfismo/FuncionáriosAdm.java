54a8bbaa952238f43c6ed7c89a6c5c0e05463f54
Debora Souza <deboral.p@hotmail.com>
2021-09-26 07:53:09 -0300

    Adicionado metodo construtor na classe FuncionárioAdm

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/FuncionáriosAdm.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/FuncionáriosAdm.java
@@ -4,4 +4,9 @@ public class FuncionáriosAdm extends Funcionários {
     private String funcaoAdm;
     private String senioridade;
 
+    public FuncionáriosAdm(String nome, String cpf, String numeroRegistro, String orgaoLoacao, double salario, String funcaoAdm, String senioridade) {
+        super(nome, cpf, numeroRegistro, orgaoLoacao, salario);
+        this.funcaoAdm = funcaoAdm;
+        this.senioridade = senioridade;
+    }
 }
