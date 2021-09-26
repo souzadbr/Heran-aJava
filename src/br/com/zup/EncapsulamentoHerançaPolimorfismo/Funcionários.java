0f6315efdfa202d5f6d35a731039db368449e56f
Debora Souza <deboral.p@hotmail.com>
2021-09-26 07:29:48 -0300

    Dado atributos a classe Funcionários

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Funcionários.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Funcionários.java
@@ -1,4 +1,10 @@
 package br.com.zup.EncapsulamentoHerançaPolimorfismo;
 
 public class Funcionários {
+    private String nome;
+    private String cpf;
+    private String numeroRegistro;
+    private String orgaoLoacao;
+    private double salario;
+
 }
