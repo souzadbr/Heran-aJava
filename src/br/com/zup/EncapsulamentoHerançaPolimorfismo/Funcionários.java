aca194612f43eb77523f975f49ef2b18bf74346d
Debora Souza <deboral.p@hotmail.com>
2021-09-26 07:51:28 -0300

    Adicionado getter e setters na classe Funcionarios

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Funcionários.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Funcionários.java
@@ -14,4 +14,44 @@ public class Funcionários {
         this.orgaoLoacao = orgaoLoacao;
         this.salario = salario;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroRegistro() {
+        return numeroRegistro;
+    }
+
+    public void setNumeroRegistro(String numeroRegistro) {
+        this.numeroRegistro = numeroRegistro;
+    }
+
+    public String getOrgaoLoacao() {
+        return orgaoLoacao;
+    }
+
+    public void setOrgaoLoacao(String orgaoLoacao) {
+        this.orgaoLoacao = orgaoLoacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
