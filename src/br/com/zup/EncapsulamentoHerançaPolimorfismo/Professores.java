96ab168991bfd279f1107db5f3187f7231f1b735
Debora Souza <deboral.p@hotmail.com>
2021-09-26 07:39:02 -0300

    Classe professores recebe herança da classe funcionarios

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
@@ -1,6 +1,6 @@
 package br.com.zup.EncapsulamentoHerançaPolimorfismo;
 
-public class Professores {
+public class Professores extends Funcionários{
     private String nivelGraduacao;
     private String disciplinaMinistrada;
     private int qtdAlunos;
