d19a98ee48fe8bdeb6eec580970a78ea524dafaa
Debora Souza <deboral.p@hotmail.com>
2021-09-26 09:09:09 -0300

    Excluido os construtores Getter e Setters, incluido a List e novo construtor

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Coordenadores.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Coordenadores.java
@@ -1,22 +1,22 @@
 package br.com.zup.EncapsulamentoHerançaPolimorfismo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coordenadores extends Funcionários {
     private Professores profSupervisionados;
 
-    public Coordenadores(String nome, String cpf, String numeroRegistro, String orgaoLoacao, double salario, Professores profSupervisionados) {
+    private List <Professores> profSupervisionado = new ArrayList<>();
+
+    public Coordenadores(String nome, String cpf, String numeroRegistro, String orgaoLoacao, double salario, Professores profSupervisionados, List<Professores> profSupervisionado) {
         super(nome, cpf, numeroRegistro, orgaoLoacao, salario);
         this.profSupervisionados = profSupervisionados;
+        this.profSupervisionado = profSupervisionado;
     }
 
-    public Professores getProfSupervisionados() {
-        return profSupervisionados;
-    }
-
-    public void setProfSupervisionados(Professores profSupervisionados) {
-        this.profSupervisionados = profSupervisionados;
-    }
     @Override
     public double aumentoSalario (){
         return getSalario()*1.05;
     }
+
 }
--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
@@ -47,6 +47,8 @@ public class Professores extends Funcionários {
     }
 
 
+
+
 }
 
 
