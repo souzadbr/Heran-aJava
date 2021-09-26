e696c6aca200cb3d9544abbfe6078ae1e6d0e09a
Debora Souza <deboral.p@hotmail.com>
2021-09-26 07:50:17 -0300

    Adicionado Construtores na Classe Funcionarios

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Funcionários.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Funcionários.java
@@ -7,4 +7,11 @@ public class Funcionários {
     private String orgaoLoacao;
     private double salario;
 
+    public Funcionários(String nome, String cpf, String numeroRegistro, String orgaoLoacao, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numeroRegistro = numeroRegistro;
+        this.orgaoLoacao = orgaoLoacao;
+        this.salario = salario;
+    }
 }
