d54229cd5c1afa1131ec25917f44cbdff2afc557
Debora Souza <deboral.p@hotmail.com>
2021-09-26 09:22:25 -0300

    adicionado metodos de add, remover e exibir prof supervisionados

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Coordenadores.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Coordenadores.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Coordenadores extends Funcionários {
     private Professores profSupervisionados;
 
-    private List <Professores> profSupervisionado = new ArrayList<>();
+    private List<Professores> profSupervisionado = new ArrayList<>();
 
     public Coordenadores(String nome, String cpf, String numeroRegistro, String orgaoLoacao, double salario, Professores profSupervisionados, List<Professores> profSupervisionado) {
         super(nome, cpf, numeroRegistro, orgaoLoacao, salario);
@@ -14,9 +14,23 @@ public class Coordenadores extends Funcionários {
         this.profSupervisionado = profSupervisionado;
     }
 
+    public void adicionarProfessor(Professores prof) {
+        profSupervisionado.add(prof);
+    }
+
+    public void exibirProfessoresSupervicionados() {
+        for (Professores referencia : profSupervisionado) {
+            System.out.println("O nome do professor é: " + referencia.getNome());
+
+        }
+    }
+
+    public void removerProfessorSupervisionado (Professores prof){
+        profSupervisionado.remove(prof);
+    }
     @Override
-    public double aumentoSalario (){
-        return getSalario()*1.05;
+    public double aumentoSalario() {
+        return getSalario() * 1.05;
     }
 
 }
