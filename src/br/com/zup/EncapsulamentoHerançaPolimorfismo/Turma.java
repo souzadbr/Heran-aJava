f2f92541282e4649f323c83ba644239365d75485
Debora Souza <deboral.p@hotmail.com>
2021-09-26 09:31:39 -0300

    Add metodo construtor vazio, getter e setters

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Turma.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Turma.java
@@ -1,4 +1,26 @@
 package br.com.zup.EncapsulamentoHerançaPolimorfismo;
 
 public class Turma {
+    private int quantidadeDeAluno;
+    private int serie;
+
+    public Turma() {
+
+    }
+
+    public int getQuantidadeDeAluno() {
+        return quantidadeDeAluno;
+    }
+
+    public void setQuantidadeDeAluno(int quantidadeDeAluno) {
+        this.quantidadeDeAluno = quantidadeDeAluno;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
 }
