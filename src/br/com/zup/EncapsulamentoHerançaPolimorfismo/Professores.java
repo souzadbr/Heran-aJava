b1624f455a3a9a26a1283288c76881965e04a7a2
Debora Souza <deboral.p@hotmail.com>
2021-09-26 07:31:54 -0300

    Dado atributos a classe Professores

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
@@ -1,4 +1,8 @@
 package br.com.zup.EncapsulamentoHerançaPolimorfismo;
 
 public class Professores {
+    private String nivelGraduacao;
+    private String disciplinaMinistrada;
+    private int qtdAlunos;
+    private int qtdTurmas;
 }
