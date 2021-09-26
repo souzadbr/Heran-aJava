db372da68361596520d8165dc6d492a17e7cc176
Debora Souza <deboral.p@hotmail.com>
2021-09-26 07:47:59 -0300

    Getters e Setters add na classe Professor

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Professores.java
@@ -12,4 +12,36 @@ public class Professores extends Funcionários{
         this.qtdAlunos = qtdAlunos;
         this.qtdTurmas = qtdTurmas;
     }
+
+    public String getNivelGraduacao() {
+        return nivelGraduacao;
+    }
+
+    public void setNivelGraduacao(String nivelGraduacao) {
+        this.nivelGraduacao = nivelGraduacao;
+    }
+
+    public String getDisciplinaMinistrada() {
+        return disciplinaMinistrada;
+    }
+
+    public void setDisciplinaMinistrada(String disciplinaMinistrada) {
+        this.disciplinaMinistrada = disciplinaMinistrada;
+    }
+
+    public int getQtdAlunos() {
+        return qtdAlunos;
+    }
+
+    public void setQtdAlunos(int qtdAlunos) {
+        this.qtdAlunos = qtdAlunos;
+    }
+
+    public int getQtdTurmas() {
+        return qtdTurmas;
+    }
+
+    public void setQtdTurmas(int qtdTurmas) {
+        this.qtdTurmas = qtdTurmas;
+    }
 }
