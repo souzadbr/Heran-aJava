e648d9c5648984a6ce7ad491b3ae9c34a8dfdd45
Debora Souza <deboral.p@hotmail.com>
2021-09-26 08:26:53 -0300

    Adicionado Contrutor na classe Coordenadores

--- src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Coordenadores.java
+++ src/br/com/zup/EncapsulamentoHerançaPolimorfismo/Coordenadores.java
@@ -2,4 +2,9 @@ package br.com.zup.EncapsulamentoHerançaPolimorfismo;
 
 public class Coordenadores extends Funcionários {
     private Professores profSupervisionados;
+
+    public Coordenadores(String nome, String cpf, String numeroRegistro, String orgaoLoacao, double salario, Professores profSupervisionados) {
+        super(nome, cpf, numeroRegistro, orgaoLoacao, salario);
+        this.profSupervisionados = profSupervisionados;
+    }
 }
