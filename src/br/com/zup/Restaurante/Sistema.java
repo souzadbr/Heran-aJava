a7edbfb8075d2ee8191dd7d02850d2b4b51db21f
Debora Souza <deboral.p@hotmail.com>
2021-09-28 18:24:17 -0300

    Criando o método capturar dados para automatizar o Scanner

--- src/br/com/zup/Restaurante/Sistema.java
+++ src/br/com/zup/Restaurante/Sistema.java
@@ -1,4 +1,12 @@
 package br.com.zup.Restaurante;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    //Método que automatiza entrada de dados
+    private static Scanner capsturaDados(String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
